84e350d4d6767fc2a19f932da685f570841f6213
Waseem Ansar <waseemansar@outlook.com>
2021-08-19 20:44:54 +0400

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -22,9 +22,41 @@ router.get('/', auth, async (req, res) => {
 	}
 });
 
-// @route    POST users/login
-// @desc     Login a user
-// @access   Public
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Login a user and get jwt token in response
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: User successfully logged in
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjExZTM5ODMyNzQwNTE2YjVlMWM2ZmRiIiwicm9sZSI6ImNsaWVudCJ9LCJpYXQiOjE2MjkzOTA3MjMsImV4cCI6MTYzMjk5MDcyM30.MvkOETRhfWUNl7iFM1UTJW_4vaa3X-ztx-kSoQShiHU
+ *                 expiresIn:
+ *                   type: number
+ *                   example: 3600000
+ *       400:
+ *         description: Some validation errors if email and password is not provided
+ *       500:
+ *         description: Some server error
+ */
 router.post(
 	'/login',
 	[check('email', 'Email is required').isEmail(), check('password', 'Password is required').not().isEmpty()],
@@ -56,9 +88,66 @@ router.post(
 	},
 );
 
-// @route    POST users/register
-// @desc     Register a user
-// @access   Public
+/**
+ * @swagger
+ * /users/register:
+ *   post:
+ *     summary: Register a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               gender:
+ *                 type: string
+ *                 description: M or F
+ *               phonenumber:
+ *                 type: number
+ *               role:
+ *                 type: string
+ *                 description: admin or client
+ *     responses:
+ *       200:
+ *         description: User successfully registered
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 _id:
+ *                   type: string
+ *                   example: 611e88a518a1007722efe893
+ *                 name:
+ *                   type: string
+ *                   example: Waseem Ansar
+ *                 email:
+ *                   type: number
+ *                   example: example@test.com
+ *                 gender:
+ *                   type: string
+ *                   example: M
+ *                 phonenumber:
+ *                   type: number
+ *                   example: 971501234567
+ *                 role:
+ *                   type: string
+ *                   example: client
+ *                 createdAt:
+ *                   type: string
+ *                   example: 2021-08-19T16:41:38.308Z
+ *       400:
+ *         description: Some validation errors if email and password is not provided
+ *       500:
+ *         description: Some server error
+ */
 router.get(
 	'/register',
 	[
@@ -86,7 +175,7 @@ router.get(
 			user = new User({ name, email, password, gender, phonenumber, role });
 			await user.save();
 
-			res.send(user);
+			res.json(await User.findById(user.id).select('-password -updatedAt -__v'));
 		} catch (err) {
 			console.error(`Server Error -- ${err.message}`);
 			res.status(500).json({ errors: [{ msg: 'Something went wrong' }] });
